84712a91dd1ef7231d3a3b0e2efd3bcf2bd1db00
Charlotte-Ewie <cha.gautron@gmail.com>
2023-04-18 10:20:07 +0200

    Ajout du middleware dans l'index du store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import { createStore } from 'redux';    // On importe createStore depuis redux
 
 import reducer from './reducer';        // On importe notre fonction reducer
+import authMiddleware from '../middlewares/authMiddlewares';
 
-const store = createStore(reducer);     // On créé un store et on lui passe notre reducer en argument
+const store = createStore(reducer, applyMiddleware(authMiddleware));     // On créé un store et on lui passe notre reducer en argument
 
 export default store;
